2576c228dd808f0711609edf78e28ce7360b369c
Raman Sehgal <sc.ramansehgal@gmail.com>
2014-02-27 12:23:23 +0530

    Now doing actual dense Vector addition.

--- examples/tests/denseVectorAddition.cpp
+++ examples/tests/denseVectorAddition.cpp
@@ -11,42 +11,34 @@
 
 using blaze::StaticVector;
 using blaze::DynamicVector;
-
+using namespace blaze;
 int main()
 {
 int n=10000,N=0;
-int iter=50;
-double testArray[]={4,-2,5};
-/*
-for(int i=1;i<=iter;i++)
-{
-N=n*3*i;
-DynamicVector<double> a( N ), b( N ), c( N );
-// ... Initialization of the vectors
+int iter=30;
+int store=1;
+//double testArray[]={4,-2,5};
 
-StopWatch tmr;
-double Tacc=0.0;
-
-tmr.getOverhead(100);
-Tacc=0.0;
-tmr.Start();
-c = a + b;
-tmr.Stop();
-Tacc=tmr.getDeltaSecs();
-std::cout<<"Execution Time N="<<N<<"  :  "<<Tacc<<std::endl;
-}
-*/
-std::cout<<"==================================================================="<<std::endl;
-std::cout<<"Starting Using Vector3DFast Class"<<std::endl;
-std::cout<<"==================================================================="<<std::endl;
-//Using new Libraries
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Using Vector3DFast Library
 Vector3DFast fv;
 std::cout << fv <<std::endl;
 //Vector3DFast bv,cv,av,sumv;
-Vector3DFast sumv;
+Vector3DFast sumv; //for storing sum using Vector3DFast
+DynamicVector<double,rowVector> sv(3); //for storing sum using Blaze
+//StaticVector<double,3UL,rowVector> sv;
+DynamicVector<double> dc;
 for(int i=1;i<=iter;i++)
 {
 N=n*i;
+double *testArray=new double[N*3];
+double *Vector3DFastArray=new double[N*3];
+double *BlazeArray=new double[N*3];
+double *denseBlazeArray=new double[N*3];
+for(int k=0 ; k<3*N ; k++)
+{
+ testArray[k]=1.2;
+}
 
 StopWatch tmr;
 double Tacc=0.0;
@@ -55,77 +47,92 @@ Tacc=0.0;
 tmr.Start();
 for(int j=0;j<N;j++)
 {
-Vector3DFast bv(2,5,-3),cv;//,av(4,-2,5);
-Vector3DFast av(testArray);
+Vector3DFast av(testArray+(3*j));
+Vector3DFast bv(testArray+(3*j));
+Vector3DFast cv;
 cv=av+bv;
-sumv+=cv;
+if(store)
+{
+*(Vector3DFastArray+(3*j)+0)=cv.GetX();
+*(Vector3DFastArray+(3*j)+1)=cv.GetY();
+*(Vector3DFastArray+(3*j)+2)=cv.GetZ();
+}
+//sumv+=cv;
 }
 tmr.Stop();
 Tacc=tmr.getDeltaSecs();
-std::cout<<"Execution Time N="<<N*3<<"  :  "<<Tacc<<std::endl;
-//std::cout << cv <<std::endl;
-}
-std::cout<<sumv<<std::endl;
-//std::cout << bv <<std::endl;
-//Vector3DFast cv(0,0,0);
+std::cout<<"Execution Time N="<<N*3<<"  :  "<<Tacc<< " :::: ";
 
-/*
-std::cout<<"==================================================================="<<std::endl;
-std::cout<<"Starting Using Vector3DFast Class"<<std::endl;
-std::cout<<"==================================================================="<<std::endl;
-//Using new Libraries
-Vector3DFast fv;
-std::cout << fv <<std::endl;
-//Vector3DFast bv,cv,av,sumv;
-Vector3DFast sumv;
-for(int i=1;i<=iter;i++)
-{
-N=n*i;
-Vector3DFast av=new Vector3DFast();
-Vector3DFast bv=new Vector3DFast();
-Vector3DFast cv=new Vector3DFast();
-StopWatch tmr;
-double Tacc=0.0;
-tmr.getOverhead(100);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Below code uses Blaze and utilizing its 3D vectors to do dense vector addition
 Tacc=0.0;
-tmr.Start();
-
-cv=av+bv;
-
-tmr.Stop();
-Tacc=tmr.getDeltaSecs();
-std::cout<<"Execution Time N="<<N*3<<"  :  "<<Tacc<<std::endl;
-//std::cout << cv <<std::endl;
-}
-*/
-std::cout<<"==================================================================="<<std::endl;
-std::cout<<"Starting Using BLAZE Manually"<<std::endl;
-std::cout<<"==================================================================="<<std::endl;
-//Using new Libraries
-DynamicVector<double> sv(3);
-for(int i=1;i<=iter;i++)
-{
-N=n*i;
-StopWatch tmr;
-double Tacc=0.0;
-
 tmr.getOverhead(100);
 Tacc=0.0;
 tmr.Start();
 for(int j=0;j<N;j++)
 {
 //DynamicVector<double> a( 3 ), b( 3 ), c( 3 );
-StaticVector<int,3UL> a( testArray );
-StaticVector<int,3UL> b( 2, 5, -3 );
-StaticVector<int,3UL> c;
+StaticVector<double,3UL,rowVector> a(3UL, testArray+(3*j));
+//StaticVector<double,3UL> b( 2, 5, -3 );
+StaticVector<double,3UL,rowVector> b( 3UL, testArray+(3*j) );
+StaticVector<double,3UL,rowVector> c;
 c=a+b;
-sv+=c;
+//sv+=c;
+if(store)
+{
+*(BlazeArray+(3*j)+0)=c[0];
+*(BlazeArray+(3*j)+1)=c[1];
+*(BlazeArray+(3*j)+2)=c[2];
+}
 }
 tmr.Stop();
 Tacc=tmr.getDeltaSecs();
-std::cout<<"Execution Time N="<<N*3<<"  :  "<<Tacc<<std::endl;
+//std::cout<<Tacc<<std::endl;
+std::cout<<Tacc<<" :::: "; //std::endl;
+//std::cout<<sumv<<"  ::  "<<sv;//<<std::endl;
 
-//std::cout
-} 
-std::cout<<sv<<std::endl;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Utilizing dense vector mechanism of Blaze itself
+Tacc=0.0;
+tmr.getOverhead(100);
+Tacc=0.0;
+tmr.Start();
+const int sz=N*3;
+//StaticVector<double,sz> da(sz, testArray);
+////StaticVector<double,3UL> db( 2, 5, -3 );
+//StaticVector<double,sz> db( sz, testArray );
+//StaticVector<double,sz> dc;
+
+DynamicVector<double> da(sz, testArray);
+//StaticVector<double,3UL> db( 2, 5, -3 );
+DynamicVector<double> db( sz, testArray );
+//DynamicVector<double> dc;
+dc=da+db;
+if(store)
+{
+for(int j=0;j<3*N;j++)
+{
+*(denseBlazeArray+j)=dc[j];
+}
+}
+tmr.Stop();
+Tacc=tmr.getDeltaSecs();
+std::cout<<Tacc<<"  :::: ";//std::endl;
+std::cout<<sumv<<"  ::  "<<sv;//<<std::endl;
+
+//Validating the results.
+for(int k=0 ; k<3*N ; k++)
+  {
+	if( (Vector3DFastArray[k]-denseBlazeArray[k]) || (Vector3DFastArray[k]-BlazeArray[k]) || (denseBlazeArray[k]-BlazeArray[k]) )
+	    std::cout<<"Value Differs"<<std::endl;
+  }
+}
+
+std::cout<<"******* Value ********"<<std::endl;
+//std::cout<<sumv<<" :::: "<<sv<<std::endl;
+//std::cout<<"----------------------"<<std::endl;
+//std::cout<<dc<<std::endl;
+
 }
